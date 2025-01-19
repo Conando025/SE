2bf0b27825e42adde89c7a04833ccc02c11ba78f
Conando <conando025@protonmail.com>
2025-01-19 20:48:34 +0100

    added Tests

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,20 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    // List of primes directly from the OEIS A000040
+    int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271};
+    for (int prime: primes) {
+      assertTrue("Correctly identify a prime", PrimeCheck.isPrime(prime));
+    }
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    // List of highly composize numbers directly from the OEIS A002182 excluding 1 and 20
+    int[] composite_numbers = {4, 6, 12, 24, 36, 48, 60, 120, 180, 240, 360, 720, 840, 1260, 1680, 2520, 5040, 7560, 10080, 15120, 20160, 25200, 27720, 45360, 50400, 55440, 83160, 110880, 166320, 221760, 277200, 332640, 498960, 554400, 665280, 720720, 1081080, 1441440, 2162160};
+    for (int composite: composite_numbers) {
+      assertFalse("Correctly identify non primes", PrimeCheck.isPrime(composite));
+    }
   }
 }
